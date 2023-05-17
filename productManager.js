2abda1b50a1f511a9e0ca301b14100748a90c486
AlanGomez2022 <alanagustingomez@gmail.com>
2023-05-17 03:48:10 -0300

    3er commit

--- productManager.js
+++ productManager.js
@@ -16,9 +16,39 @@ class ProductManager{
     
     addProduct = async (product) => {
         const productos = await this.getProducts()
-        const producto = {id:this.generateId(productos),...product}
-        productos.push(producto)
+        console.log(productos)
+        productos.push({id:this.generateId(productos)})
         return await fs.promises.writeFile(this.#path,JSON.stringify(productos,null,'\t'))
+    }
+    updateProduct = async (id, producto)=> {
+       
+        const productos = await this.getProducts()
+        console.log('llego aca')
+        let encontrado = await this.getProductById(id)
+        if( encontrado !== false){
+            let nuevo = { // creo un nuevo producto guardandome el ID anterior
+                id:      id,
+                title:   producto.title,
+                price:   producto.price,
+                thumnail:producto.thumnail,
+                code:    producto.code,
+                stock:   producto.stock
+            }
+            let nuevosProductos = productos.filter(produ =>produ.code!==id) // filtro y saco al que no me sirve
+           
+            nuevosProductos.push(nuevo)// agrego el producto actualizado al array
+            
+            return await fs.promises.writeFile(this.#path, JSON.stringify(nuevosProductos,null,'\t')) //grabo en archivo
+        }return false
+       
+    }
+    deleteProduct = async (id) => {
+        const productos = await this.getProducts()
+        const yaEliminado = productos.filter(product => product.code!==id)
+        return await fs.promises.writeFile(this.#path, JSON.stringify(yaEliminado,null,'\t')) //grabo en archivo
+
+
+
     }
 
     getProducts = async () =>{
@@ -30,16 +60,24 @@ class ProductManager{
         let encontrado = productos.find((producto)=> producto.code===id)
         if (encontrado===undefined){
             console.log('el producto no existe')
+            return false // si no lo encontre devuelve falso
         }else{
             console.log(encontrado)
+            return encontrado // si lo encontre devuelve el objeto
         }
     }
+    
 
 }
+
+
+/// PRUEBAS DEL PROGRAMA---------------------------------------------------------------------------
+
 const fileName = './productos.json'
 const manager = new ProductManager(fileName)
 
 const newProduct = {
+    
     title:'Jack Daniels',
     price: 12500,
     thumnail: 'image.png',
@@ -49,3 +87,14 @@ const newProduct = {
 }
 manager.addProduct(newProduct)
 manager.getProductById(12300)
+
+const toUpdateProduct = {
+    title:'Jim Bean',
+    price: 10000,
+    thumnail: 'image.png',
+    code:12200,
+    stock:500
+
+}
+manager.updateProduct(12200,toUpdateProduct)
+manager.deleteProduct(12500)
